8cf93fd2a57a9b486625a2e1bf2fb087626da013
zhaoyk10 <zhaoyk10@gmail.com>
2014-10-30 00:53:38 +0800

    dired+ 显示bug; 使用 ggrep

--- lisp/init-misc-my.el
+++ lisp/init-misc-my.el
@@ -11,7 +11,7 @@
 (setq auto-save-default nil)
 
 (global-set-key "\M-s" 'save-buffer)
-
+(global-set-key "\M-L" 'indent-buffer)
 ; evil-key-binding
 ; https://github.com/mbriggs/.emacs.d/blob/master/init/init-keymaps.el
 (evil-define-key 'normal global-map
@@ -20,6 +20,7 @@
 
 (evil-define-key 'insert global-map
   (kbd "M-J") 'evil-join
+  (kbd "M-<backspace>") 'kill-whole-line
   (kbd "M-v") 'yank
   (kbd "M-h") 'evil-backward-char
   (kbd "M-l") 'evil-forward-char
@@ -27,5 +28,10 @@
   (kbd "M-k") 'evil-previous-line
   (kbd "M-S-<return>") 'evil-open-above
   (kbd "M-<return>") 'evil-open-below)
+
+; in macos change helm grep to ggrep
+(when (and *is-a-mac* (executable-find "ggrep"))
+  (setq helm-grep-default-command "ggrep -a -d skip %e -n%cH -e %p %f"
+        helm-grep-default-recurse-command "ggrep -a -d recurse %e -n%cH -e %p %f"))
 
 (provide 'init-misc-my)
