c8de9f802eb9de37d206a14d49d6fe39974638da
zhaoyk10 <zhaoyk10@gmail.com>
2014-11-20 19:23:26 +0800

    使用 mono 的 xbuild

--- lisp/init-csharp-mode.el
+++ lisp/init-csharp-mode.el
@@ -1,5 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
-(add-hook 'csharp-mode-hook
-          (lambda ()
-            (local-set-key (kbd "{") 'c-electric-brace)))
+
+(custom-set-variables '(csharp-msbuild-tool "xbuild"))
+
+(defun my-csharp-common-mode-fn ()
+  (local-set-key (kbd "{") 'c-electric-brace))
+
+(add-hook 'csharp-mode-hook 'my-csharp-common-mode-fn)
 (provide 'init-csharp-mode)
