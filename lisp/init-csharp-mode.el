1b44246794dc0be4c26c6f00643fbee6d086ba51
zhaoyk10 <zhaoyk10@gmail.com>
2014-11-20 18:33:54 +0800

    csharp-mode { bug

--- lisp/init-csharp-mode.el
+++ lisp/init-csharp-mode.el
@@ -1,2 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
+(add-hook 'csharp-mode-hook
+          (lambda ()
+            (local-set-key (kbd "{") 'c-electric-brace)))
 (provide 'init-csharp-mode)
